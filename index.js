cc5f03800b1785310ee53755374fdd6209754473
Wes Griffin <themisguidedidiot@gmail.com>
2019-06-27 01:38:55 -0700

    finally got the html to load at right pathname

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const { createServer } = require('http');
 const { parse } = require('url');
-
+const fs = require('fs');
 
 const server = createServer((req, res) => {
   const { pathname } = parse(req.url);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.write('working');
-  res.end();
+  if(pathname === '/index.html') {
+    fs.readFile('public/index.html', (err,  data) => {  
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
+
 server.listen(7890, () => {
-  console.log('listening on 7890');
+  console.log('listening to 7890');
 });
+
+
