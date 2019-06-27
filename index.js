921dc2e4a5444eb52f2a63e874d30df293aab911
Wes Griffin <themisguidedidiot@gmail.com>
2019-06-27 01:53:44 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ const server = createServer((req, res) => {
   const { pathname } = parse(req.url);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+
   if(pathname === '/index.html') {
     fs.readFile('public/index.html', (err,  data) => {  
-      res.write(data);
-      res.end();
+      res.end(data);
     });
   } else {
     res.statusCode = 404;
-    res.end();
+    res.end('FILE NOT FOUND');
   }
 });
-
 
 server.listen(7890, () => {
   console.log('listening to 7890');
